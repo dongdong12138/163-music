9e8c7e90230525e43199f1f561a7510fa919a1a2
dongdong <286084845@qq.com>
2019-02-22 21:16:05 +0800

    歌曲表单

---
+++ src/js/songForm.js
@@ -0,0 +1,94 @@
+{
+  let view = {
+    el: ".page > main",
+    init() {
+      this.$el = $(this.el);
+    },
+    template: `
+            <h1>新建歌曲</h1>
+            <form action="" class="form">
+                <div class="row">
+                    <label>歌名：</label>
+                    <input name="name" type="text" value="__name__">
+                </div>
+                <div class="row">
+                    <label>歌手：</label>
+                    <input name="singer" type="text" value="__singer__">
+                </div>
+                <div class="row">
+                    <label>外链：</label>
+                    <input name="url" type="text" value="__url__">
+                </div>
+                <div class="row actions">
+                    <button type="submit">保存</button>
+                </div>
+            </form>
+        `,
+    render(data = {}) {
+      let placeholders = ['name', 'url', 'singer', 'id'];
+      let html = this.template;
+      placeholders.map(string => {
+        html = html.replace(`__${string}__`, data[string] || "");
+      });
+      $(this.el).html(html);
+    },
+    reset() {
+        this.render({})
+    }
+  };
+  let model = {
+    data: {
+      name: "",
+      singer: "",
+      url: "",
+      id: ""
+    },
+    create(data) {
+      // 声明类型
+      var Song = AV.Object.extend("Song");
+      // 新建对象
+      var song = new Song();
+      // 设置名称
+      song.set("name", data.name);
+      song.set("singer", data.singer);
+      song.set("url", data.url);
+      return song.save().then(
+        newSong => {
+            let {id, attributes} = newSong
+            Object.assign(this.data, {id, ...attributes})
+        },
+        error => {
+          console.error(error);
+        }
+      );
+    }
+  };
+  let controller = {
+    init(view, model) {
+      this.view = view;
+      this.view.init();
+      this.model = model;
+      this.view.render(this.model.data);
+      this.bindEvents();
+      window.eventHub.on("upload", data => {
+        this.view.render(data);
+      });
+    },
+    bindEvents() {
+      this.view.$el.on("submit", "form", e => {
+        e.preventDefault();
+        let needs = "name singer url".split(" ");
+        let data = {};
+        needs.map(string => {
+          data[string] = this.view.$el.find(`[name="${string}"]`).val();
+        });
+        this.model.create(data)
+            .then(() => {
+                this.view.reset()
+                window.eventHub.emit('create', this.model.data)
+            })
+      });
+    }
+  };
+  controller.init(view, model);
+}
