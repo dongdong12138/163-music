14d0e91101cbdde68af48362c668d00530ab3ec1
dongdong <286084845@qq.com>
2019-02-28 14:39:05 +0800

    添加loading

--- src/js/uploadSong.js
+++ src/js/uploadSong.js
@@ -44,11 +44,13 @@
                   },
                   BeforeUpload: function(up, file) {
                     // 每个文件上传前，处理相关的事情
+                    window.eventHub.emit('beforeUpload')
                   },
                   UploadProgress: function(up, file) {
                     // 每个文件上传时，处理相关的事情
                   },
                   FileUploaded: function(up, file, info) {
+                      window.eventHub.emit('afterUpload')
                     var domain = up.getOption('domain');
                     var response = JSON.parse(info.response);
                     var sourceLink = 'http://' + domain +"/"+ encodeURIComponent(response.key); // 获取上传成功后的文件的Url
